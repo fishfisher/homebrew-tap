129ad9fea4ad7c91026f35ad3919c265fb65de05
Erik Fisher <erik.fisher@gmail.com>
2026-02-09 14:25:02 +0100

    Update oda to v1.0.3

--- oda.rb
+++ oda.rb
@@ -3,21 +3,21 @@ require_relative "lib/private_strategy"
 class Oda < Formula
   desc "CLI for ordering groceries from Oda.com"
   homepage "https://github.com/fishfisher/oda-cli"
-  version "1.0.2"
+  version "1.0.3"
   license "MIT"
 
   depends_on :macos
 
   on_arm do
-    url "https://github.com/fishfisher/oda-cli/releases/download/v1.0.2/oda-darwin-arm64",
+    url "https://github.com/fishfisher/oda-cli/releases/download/v1.0.3/oda-darwin-arm64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "0548286aba81cdc8f9a46989bf27a727fc643b5181a64fa7b591a903b0ce58e4"
+    sha256 "257351637ad9537e21433063897a68fb65c7e2cc8768f6e221d08365ca057b18"
   end
 
   on_intel do
-    url "https://github.com/fishfisher/oda-cli/releases/download/v1.0.2/oda-darwin-amd64",
+    url "https://github.com/fishfisher/oda-cli/releases/download/v1.0.3/oda-darwin-amd64",
         using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "42443cf3b96ad8ba41cb9b2234c139a6f44300c7c168db9e11c26240bba6339f"
+    sha256 "563a0f21e0b7628814a645c8879e07a99d8df1cb67d4831875493cc258b829e7"
   end
 
   def install
