43bceeb1e03c56d38be0ff70280c91c31442a265
Erik Fisher <erik.fisher@gmail.com>
2026-02-09 13:04:54 +0100

    Update norsk-tipping to v1.0.1

--- norsk-tipping.rb
+++ norsk-tipping.rb
@@ -1,23 +1,19 @@
-require_relative "lib/private_strategy"
-
 class NorskTipping < Formula
   desc "CLI for buying lottery tickets from Norsk Tipping"
   homepage "https://github.com/fishfisher/norsk-tipping-cli"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   depends_on :macos
 
   on_arm do
-    url "https://github.com/fishfisher/norsk-tipping-cli/releases/download/v1.0.0/norsk-tipping-darwin-arm64",
-        using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "f4d7a059b78f2b10792b7492ab871888435c516b33e4baec6c0a2ce6a4c05df9"
+    url "https://github.com/fishfisher/norsk-tipping-cli/releases/download/v1.0.1/norsk-tipping-darwin-arm64"
+    sha256 "9346d8be70378cebbda2b61b264f32667f7c1a36743bb35df41fdf079105fef3"
   end
 
   on_intel do
-    url "https://github.com/fishfisher/norsk-tipping-cli/releases/download/v1.0.0/norsk-tipping-darwin-amd64",
-        using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "8faab4aecac534a7868bd9f467f5c87d2493eb6ea098161c6dff99cdfb3659f6"
+    url "https://github.com/fishfisher/norsk-tipping-cli/releases/download/v1.0.1/norsk-tipping-darwin-amd64"
+    sha256 "68ce904f6b5c70639992a6c40ff3bfeb3287b7d459768aee2fb2232c1d0d24e3"
   end
 
   def install
