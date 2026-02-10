f597624ece9ccb2e4bb500556593fc4038bac403
Erik Fisher <erik.fisher@gmail.com>
2026-02-10 11:49:02 +0100

    Update apple-reminders to 0.3.1

--- apple-reminders.rb
+++ apple-reminders.rb
@@ -3,15 +3,15 @@ require_relative "lib/private_strategy"
 class AppleReminders < Formula
   desc "CLI for managing Apple Reminders from the terminal"
   homepage "https://github.com/fishfisher/apple-reminders"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   depends_on :macos
   depends_on arch: :arm64
 
-  url "https://github.com/fishfisher/remindctl/releases/download/v0.3.0/apple-reminders-macos.zip",
+  url "https://github.com/fishfisher/remindctl/releases/download/v0.3.1/apple-reminders-macos.zip",
       using: GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "675554917b86c4148e93739b28520f08b4abb84666ba962d33977f646703b3ca"
+  sha256 "42874983c4950f6b8c55fb745dba28b5098f8f0d5306ae54b24d0c8e7f963ab0"
 
   def install
     bin.install "apple-reminders"
