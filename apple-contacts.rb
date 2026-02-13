f7a52059922f6b38abcaef2c931b686ce3d73618
Erik Fisher <erik.fisher@gmail.com>
2026-02-13 23:13:59 +0100

    Update apple-contacts to v0.3.1

--- apple-contacts.rb
+++ apple-contacts.rb
@@ -1,14 +1,14 @@
 class AppleContacts < Formula
   desc "CLI for searching and querying Apple Contacts using native Contacts Framework"
   homepage "https://github.com/fishfisher/apple-contacts"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   depends_on :macos
   depends_on arch: :arm64
 
-  url "https://github.com/fishfisher/apple-contacts/releases/download/v0.3.0/apple-contacts-darwin-arm64.tar.gz"
-  sha256 "759e1ef17e985e8433a2fac1486130ff6123a47f0d611917a54c603e867f2b82"
+  url "https://github.com/fishfisher/apple-contacts/releases/download/v0.3.1/apple-contacts-darwin-arm64.zip"
+  sha256 "d673f3f934c950b821220d07a11d38d35f83a4c11927adeb83487c6323527be2"
 
   def install
     bin.install "apple-contacts"
