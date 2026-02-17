507cd0fbb7fd11c9bdeca52eefd7c083fadf187f
Erik Fisher <erik.fisher@gmail.com>
2026-02-17 11:11:13 +0100

    Update homeyctl to v1.2.0 (rebuild)

--- homeyctl.rb
+++ homeyctl.rb
@@ -1,28 +1,21 @@
-# typed: false
-# frozen_string_literal: true
-
 require_relative "lib/private_strategy"
+
 class Homeyctl < Formula
-  desc "CLI for controlling Homey smart home devices"
+  desc "CLI for controlling Homey smart home"
   homepage "https://github.com/fishfisher/homeyctl"
   version "1.2.0"
-  depends_on :macos
+  license "MIT"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/fishfisher/homeyctl/releases/download/v1.2.0/homeyctl-darwin-amd64", using: FishfisherPrivateReleaseDownloadStrategy
-    sha256 "9cb04744c83413603619f282b8d2cfda105fad2035db4081c5e5445303592acf"
-
-    def install
-      bin.install Dir["homeyctl*"].first => "homeyctl"
-    end
-  end
   if Hardware::CPU.arm?
     url "https://github.com/fishfisher/homeyctl/releases/download/v1.2.0/homeyctl-darwin-arm64", using: FishfisherPrivateReleaseDownloadStrategy
-    sha256 "8bdd2d693101b87241039748fc498c29059274c4984f6e282506d45fe41cea67"
+    sha256 "024df630a7ed8e9b4216a120080881aa6aa8fa6880b17e505239005265c9b847"
+  else
+    url "https://github.com/fishfisher/homeyctl/releases/download/v1.2.0/homeyctl-darwin-amd64", using: FishfisherPrivateReleaseDownloadStrategy
+    sha256 "d691c6a9585311a00e2f8e03c24e7bc599e2c3f25eaca2d527929486363bfb3b"
+  end
 
-    def install
-      bin.install Dir["homeyctl*"].first => "homeyctl"
-    end
+  def install
+    bin.install Dir["homeyctl*"].first => "homeyctl"
   end
 
   test do
