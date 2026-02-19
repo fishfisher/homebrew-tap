072b0e05789a3107502ed4c6fc9e73fed63a0458
Erik Fisher <erik.fisher@gmail.com>
2026-02-19 10:31:26 +0100

    Fix sonoscli formula: add private download strategy

--- sonoscli.rb
+++ sonoscli.rb
@@ -1,3 +1,4 @@
+require_relative "lib/private_strategy"
 class Sonoscli < Formula
   desc "Control Sonos speakers from the command line"
   homepage "https://github.com/fishfisher/sonoscli"
@@ -7,7 +8,7 @@ class Sonoscli < Formula
   depends_on :macos
   depends_on arch: :arm64
 
-  url "https://github.com/fishfisher/sonoscli/releases/download/v0.2.0/sonoscli_0.2.0_darwin_arm64.zip"
+  url "https://github.com/fishfisher/sonoscli/releases/download/v0.2.0/sonoscli_0.2.0_darwin_arm64.zip", using: FishfisherPrivateReleaseDownloadStrategy
   sha256 "081bb71ec33c44bd8cce92529555a2df350b1ac9faa6e44d11c2bcf7d2a31537"
 
   def install
