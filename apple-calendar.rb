ddeaeae0e878abad5e907a11191da1daefeb1a79
Erik Fisher <erik.fisher@gmail.com>
2026-02-23 18:55:02 +0100

    Update apple-calendar to 2.1.0

--- apple-calendar.rb
+++ apple-calendar.rb
@@ -2,14 +2,14 @@ require_relative "lib/private_strategy"
 class AppleCalendar < Formula
   desc "CLI for managing Apple Calendar events from the command line"
   homepage "https://github.com/fishfisher/apple-calendar-cli"
-  version "2.0.1"
+  version "2.1.0"
   license "MIT"
 
   depends_on :macos
   depends_on "node"
 
-  url "https://github.com/fishfisher/apple-calendar-cli/releases/download/v2.0.1/fishfisher-apple-calendar-2.0.1.tgz", using: FishfisherPrivateReleaseDownloadStrategy
-  sha256 "4b326cbcd149ea85c5c909459fad5d87bdc79ce69460f57c11a97e2e51963c58"
+  url "https://github.com/fishfisher/apple-calendar-cli/releases/download/v2.1.0/fishfisher-apple-calendar-2.1.0.tgz", using: FishfisherPrivateReleaseDownloadStrategy
+  sha256 "07b316195a8aee17a5d0e0b3d69a9c56214b71332bc3f1d72de261a6ce7b06cb"
 
   def install
     system "npm", "install", *std_npm_args
